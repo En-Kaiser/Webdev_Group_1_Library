9cceacad906ba303a66fea099458552c7a75c8e8
reicdm <reicdmagpantay@gmail.com>
2026-01-21 15:00:25 +0800

    Revert "login"
    This reverts commit c24e2dd524d189c01671be292483800fd03e8eb4.

--- app/Http/Controllers/LogInController.php
+++ app/Http/Controllers/LogInController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-use App\Models\Admin;
 use App\Models\user_account;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -31,15 +30,6 @@ class LogInController extends Controller
             Auth::login($user);
             $request->session()->regenerate();
             return redirect('/home');
-        }
-
-        $admin = Admin::where('email', $email)->first();
-
-        if ($admin && Hash::check($password, $admin->password)) {
-            Auth::login($admin);
-            $request->session()->regenerate();
-
-            return redirect('/librarian/all');
         }
 
         return back()->withErrors([
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,26 +18,26 @@
             <form method="POST" action="{{ route('auth.login') }}" class="auth-form">
 
                 @csrf
-
+                
                 <h2>Welcome Back!</h2>
                 @error('email')
-                <div style="color: Yellow;">{{ $message }}</div>
+                    <div style="color: Yellow;">{{ $message }}</div>
                 @enderror
                 <input type="email" placeholder="Email" name="email" required>
                 @error('password')
-                <div style="color: Yellow;">{{ $message }}</div>
+                    <div style="color: Yellow;">{{ $message }}</div>
                 @enderror
                 <input type="password" placeholder="Password" name="password" required>
 
                 <div class="divider"></div>
                 @error('course')
-                <div style="color: Yellow;">{{ $message }}</div>
+                    <div style="color: Yellow;">{{ $message }}</div>
                 @enderror
-                <select class='form-select' name="course">
+                <select class='form-select' name="course" required>
                     <option disabled selected>Choose Course</option>
-                    @foreach($courses as $course)
-                    <option value="{{$course->course_id}}">{{$course->name}}</option>
-                    @endforeach
+                        @foreach($courses as $course)
+                            <option value="{{$course->course_id}}">{{$course->name}}</option>
+                        @endforeach
                 </select>
 
                 <button type="submit" style="margin-top: 6rem;">Log In</button>
